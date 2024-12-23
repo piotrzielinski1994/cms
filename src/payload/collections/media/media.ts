d847b36fd77dcad494bddb1e81743403aec9f9cb
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2024-12-23 11:41:04 +0100

    Fix path to media

--- src/payload/collections/media/media.ts
+++ src/payload/collections/media/media.ts
@@ -12,7 +12,7 @@ import { anyone } from '@/payload/access/anyone';
 import { authenticated } from '@/payload/access/authenticated';
 
 const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+const root = path.resolve(process.cwd());
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -40,7 +40,7 @@ export const Media: CollectionConfig = {
   ],
   upload: {
     // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    staticDir: path.resolve(dirname, '../../../public/media'),
+    staticDir: path.join(root, 'public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
     imageSizes: [
