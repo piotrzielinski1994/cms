1ac36077f695732ddf265ee7a1d19dd29df788f7
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-03-25 20:05:54 +0100

    Update healthcheck

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 export const revalidate = 0;
 
 export async function GET() {
-  await fetch(`https://${process.env.VERCEL_URL}/admin`);
+  await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/admin`);
   return NextResponse.json({ ok: true });
 }
