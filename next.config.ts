2891bbc6e49a4e7167e4c5658c48c8009963be5d
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-03-30 13:15:08 +0200

    Move next config to ts

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,28 @@
 import { withPayload } from '@payloadcms/next/withPayload';
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+import { RemotePattern } from 'next/dist/shared/lib/image-config';
 
 const withNextIntl = createNextIntlPlugin('./src/payload/locale/request.ts');
-
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+const serverUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+  : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item);
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        };
-      }),
-    ],
+    remotePatterns: [serverUrl].map((item) => {
+      const url = new URL(item);
+      return {
+        hostname: url.hostname,
+        protocol: url.protocol.replace(':', ''),
+      } as RemotePattern;
+    }),
   },
   reactStrictMode: true,
   redirects: async () => {
     const internetExplorerRedirect = {
       destination: '/ie-incompatible.html',
-      has: [
-        {
-          type: 'header',
-          key: 'user-agent',
-          value: '(.*Trident.*)', // all ie browsers
-        },
-      ],
+      has: [{ type: 'header', key: 'user-agent', value: '(.*Trident.*)' /* all ie browsers */ }],
       permanent: false,
       source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
     };
@@ -41,17 +32,10 @@ const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgo: false,
-          },
-        },
-      ],
+      use: [{ loader: '@svgr/webpack', options: { svgo: false } }],
     });
     return config;
   },
-};
+} satisfies NextConfig;
 
 export default withNextIntl(withPayload(nextConfig));
