c46002c04d4b1eb73903d8f743cfc7287c9606e8
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-03-30 12:36:18 +0200

    Rework env.config.ts

---
+++ src/config/env.config.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  // Default
+  PAYLOAD_SECRET: z.string().min(10),
+  DATABASE_URI: z.string(),
+  NEXT_PUBLIC_SERVER_URL: z.string().url(),
+
+  // Vercel
+  BLOB_READ_WRITE_TOKEN: z.string().optional(),
+});
+
+const parsedEnv = envSchema.parse({
+  PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
+  DATABASE_URI: process.env.DATABASE_URI,
+  NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
+});
+
+const env = {
+  payloadSecret: parsedEnv.PAYLOAD_SECRET,
+  dbUri: parsedEnv.DATABASE_URI,
+  publicUrl: parsedEnv.NEXT_PUBLIC_SERVER_URL,
+  vercel: {
+    blobStorageToken: parsedEnv.BLOB_READ_WRITE_TOKEN,
+  },
+};
+
+export { env };
