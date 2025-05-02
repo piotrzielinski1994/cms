5b1d23f16b3926999bab78b5ed457a4ef0aae30a
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-05-02 23:33:08 +0200

    Mock getCachedGlobal

--- src/components/layout/header/header.test.tsx
+++ src/components/layout/header/header.test.tsx
@@ -1,16 +1,27 @@
 import { resolveServerComponent, withProviders } from '@/utils/tests';
 import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
-// import { axe } from 'vitest-axe';
+import { describe, expect, it, vi } from 'vitest';
+import { axe } from 'vitest-axe';
 import { Header } from './header';
 
+vi.mock('@/payload/utils/globals', () => ({
+  getCachedGlobal: vi.fn().mockReturnValue(
+    vi.fn().mockResolvedValue({
+      navItems: [
+        { id: '1', link: { label: 'Home', reference: { value: { path: '/' } } } },
+        { id: '2', link: { label: 'About', reference: { value: { path: '/about' } } } },
+      ],
+    }),
+  ),
+}));
+
 describe('Header', () => {
-  // it('should have no accessibility violations', async () => {
-  //   const ServerHeader = await resolveServerComponent(Header, { locale: 'en' });
-  //   const { container } = render(withProviders(<ServerHeader />));
-  //   const results = await axe(container);
-  //   expect(results).toHaveNoViolations();
-  // });
+  it('should have no accessibility violations', async () => {
+    const ServerHeader = await resolveServerComponent(Header, { locale: 'en' });
+    const { container } = render(withProviders(<ServerHeader />));
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 
   it('should match the snapshot', async () => {
     const ServerHeader = await resolveServerComponent(Header, { locale: 'en' });
