d758c8014a20762d79af24c2ac31813297fa2930
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-05-25 10:06:11 +0200

    asd

--- src/utils/next.ts
+++ src/utils/next.ts
@@ -1,8 +1,8 @@
+import { GlobalRevalidationTag } from '@/payload/utils/globals';
 import { Locale } from 'next-intl';
 import { revalidatePath, revalidateTag } from 'next/cache';
-import { GlobalSlug } from 'payload';
 
-const rebuildTag = (tag: `global__${Locale}__${GlobalSlug}` | 'sitemap'): void => {
+const rebuildTag = (tag: GlobalRevalidationTag | 'sitemap'): void => {
   return revalidateTag(tag);
 };
 
