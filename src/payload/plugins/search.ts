b1c4330a67f4930f8225f8ee0d44d7a24a914f6a
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-05-25 23:13:35 +0200

    Remove unused payload components

--- src/payload/plugins/search.ts
+++ src/payload/plugins/search.ts
@@ -1,13 +1,10 @@
-import { beforeSyncWithSearch } from '@/_old/search/beforeSync';
-import { searchFields } from '@/_old/search/fieldOverrides';
 import { searchPlugin } from '@payloadcms/plugin-search';
 
 const search = searchPlugin({
   collections: [],
-  beforeSync: beforeSyncWithSearch,
   searchOverrides: {
     fields: ({ defaultFields }) => {
-      return [...defaultFields, ...searchFields];
+      return [...defaultFields];
     },
   },
 });
