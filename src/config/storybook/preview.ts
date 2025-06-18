286d044892b8bbd377452f691d2764a06bbf06c1
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-06-19 00:29:52 +0200

    Add support for font scaling in storybook

--- src/config/storybook/preview.ts
+++ src/config/storybook/preview.ts
@@ -1,9 +1,12 @@
 import '@/app/(frontend)/[locale]/globals.scss';
 import { withThemeByDataAttribute } from '@storybook/addon-themes';
 import { fromPairs, keys } from 'ramda';
+import React from 'react';
+import { fontScales } from '../font-scales.config';
 import { Theme, themes } from '../themes.config'; // TODO: Add support for path aliases in storybook
 
 const defaultTheme: Theme = 'light';
+const defaultFontScale: keyof typeof fontScales = 'base';
 
 const decorators = [
   withThemeByDataAttribute({
@@ -11,12 +14,21 @@ const decorators = [
     defaultTheme,
     attributeName: 'data-theme',
   }),
+  (Story: React.ComponentType, context) => {
+    const scale = context.globals.fontScale as keyof typeof fontScales;
+    document.documentElement.setAttribute('data-scale', scale);
+    return React.createElement(Story);
+  },
 ];
 
 const globalTypes = {
   theme: {
     defaultValue: defaultTheme,
     toolbar: { title: 'Theme', items: keys(themes) },
+  },
+  fontScale: {
+    defaultValue: defaultFontScale,
+    toolbar: { title: 'Font Scale', items: keys(fontScales) },
   },
 };
 
