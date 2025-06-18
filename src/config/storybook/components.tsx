8441d9edde1c519f142e5c85e312a43f2b6d44a6
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-06-19 00:57:45 +0200

    asd

--- src/config/storybook/components.tsx
+++ src/config/storybook/components.tsx
@@ -4,6 +4,7 @@ import { translations } from '../locales.config';
 
 const withLocalization = (Story: ComponentType, context) => {
   const locale: Locale = context.globals.locale;
+  document.documentElement.setAttribute('data-locale', locale);
   return (
     <NextIntlClientProvider locale={locale} messages={translations[locale]}>
       <Story />
