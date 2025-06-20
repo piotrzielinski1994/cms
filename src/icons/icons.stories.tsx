f409b4032fe4e994821ee7a2adc82ac3c76ad8e8
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-06-20 22:03:19 +0200

    Rework icons story

--- src/icons/icons.stories.tsx
+++ src/icons/icons.stories.tsx
@@ -1,20 +1,19 @@
-import ContrastSvg from '@/icons/contrast.svg';
-import FontScalerSvg from '@/icons/font-scaler.svg';
-import LogoSvg from '@/icons/logo.svg';
+import * as svgs from '@/icons';
 import type { Meta, StoryObj } from '@storybook/react';
+import { toPairs } from 'ramda';
 
 const meta: Meta = {
   title: 'Components/Basic/Icons',
 };
 
+const { LogoSvg, ...restSvgs } = svgs;
+const icons = toPairs(restSvgs).map(([_, Svg], i) => <Svg key={i} />);
+
 const Render = () => {
   return (
-    <div className="grid gap-4">
+    <div className="grid gap-4 text-2xl">
       <LogoSvg />
-      <div className="flex flex-wrap gap-4">
-        <ContrastSvg />
-        <FontScalerSvg />
-      </div>
+      <div className="flex flex-wrap gap-4">{icons}</div>
     </div>
   );
 };
---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+import ContrastSvg from '@/icons/contrast.svg';
+import FontScalerSvg from '@/icons/font-scaler.svg';
+import LogoSvg from '@/icons/logo.svg';
+
+export { ContrastSvg, FontScalerSvg, LogoSvg };
