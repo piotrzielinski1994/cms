157bdc3c86f5a33c4ab734e91b6584ecfcec0b07
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-06-26 20:41:31 +0200

    Add button tests

---
+++ src/components/basic/button/button.test.tsx
@@ -0,0 +1,61 @@
+import { withProviders } from '@/utils/tests';
+import { fireEvent, render } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import { axe } from 'vitest-axe';
+import { Button, ButtonLink } from './button';
+
+describe('Button', () => {
+  it('should have no accessibility violations', async () => {
+    const { container } = render(<Button>Button</Button>);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should match the snapshot', () => {
+    const { container } = render(<Button disabled>Button</Button>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('calls onClick handler', () => {
+    const onClick = vi.fn();
+    const { getByRole } = render(<Button onClick={onClick}>Button</Button>);
+
+    fireEvent.click(getByRole('button', { name: 'Button' }));
+
+    expect(onClick).toHaveBeenCalled();
+  });
+});
+
+describe('ButtonLink', () => {
+  it('renders with default primary variant classes', () => {
+    const { getByRole } = render(withProviders()(<ButtonLink href="/test">ButtonLink</ButtonLink>));
+    const link = getByRole('link');
+    expect(link.className).toContain('bg-primary');
+  });
+
+  it('should have no accessibility violations', async () => {
+    const { container } = render(withProviders()(<ButtonLink href="/test">ButtonLink</ButtonLink>));
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should match the snapshot', () => {
+    const { container } = render(withProviders()(<ButtonLink href="/test">ButtonLink</ButtonLink>));
+    expect(container).toMatchSnapshot();
+  });
+
+  it('calls onClick handler', () => {
+    const onClick = vi.fn();
+    const { getByRole } = render(
+      withProviders()(
+        <ButtonLink href="/test" onClick={onClick}>
+          ButtonLink
+        </ButtonLink>,
+      ),
+    );
+
+    fireEvent.click(getByRole('link', { name: 'ButtonLink' }));
+
+    expect(onClick).toHaveBeenCalled();
+  });
+});
