2b0542b577238b059f55766d3348cf4cc514c036
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-06-26 21:20:49 +0200

    asd

--- src/components/basic/image/image.test.tsx
+++ src/components/basic/image/image.test.tsx
@@ -5,13 +5,13 @@ import { Image } from './image';
 
 describe('Image', () => {
   it('should have no accessibility violations', async () => {
-    const { container } = render(<Image src="/image.webp" alt="" />);
+    const { container } = render(<Image src="/image.webp" alt="Label" />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
 
   it('should match the snapshot', async () => {
-    const { container } = render(<Image src="/image.webp" alt="" />);
+    const { container } = render(<Image src="/image.webp" alt="Label" />);
     expect(container).toMatchSnapshot();
   });
 });
