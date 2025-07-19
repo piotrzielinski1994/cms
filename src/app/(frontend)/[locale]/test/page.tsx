b5c21db16e50c9bc7d493e0ae6fa0603297c37c8
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-07-19 21:27:06 +0200

    asd

--- src/app/(frontend)/[locale]/test/page.tsx
+++ src/app/(frontend)/[locale]/test/page.tsx
@@ -1,30 +1,7 @@
-import { Gallery } from '@/components/advanced/gallery/gallery';
-import { Container } from '@/components/basic/container';
-import { Section } from '@/components/basic/section';
 import { Product } from '@/features/products/components/sections/product/product';
-import placeholderDarkWebp from '@/placeholders/placeholder-dark.webp';
-import placeholderWebp from '@/placeholders/placeholder.webp';
 
 const TestPage = () => {
-  return (
-    <>
-      <Section>
-        <Container>
-          <Gallery
-            images={[
-              placeholderWebp.src,
-              placeholderDarkWebp.src,
-              placeholderWebp.src,
-              placeholderDarkWebp.src,
-              placeholderWebp.src,
-              placeholderDarkWebp.src,
-            ].map((it) => ({ src: it, alt: it }))}
-          />
-        </Container>
-      </Section>
-      <Product />
-    </>
-  );
+  return <Product />;
 };
 
 export default TestPage;
