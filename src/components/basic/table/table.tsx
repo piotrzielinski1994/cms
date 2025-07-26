3b1ddc045d0e6b4f74a1ea81aea4c0b1f2982f88
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-07-26 12:44:04 +0200

    Add support for table overflow

--- src/components/basic/table/table.tsx
+++ src/components/basic/table/table.tsx
@@ -12,62 +12,64 @@ type TableProps = HTMLAttributes<HTMLTableElement> & {
 const Table = ({ header, body, footer = [], ...props }: TableProps) => {
   const hasEvenElements = body.length % 2 === 0;
   return (
-    <table {...props} className={cn('w-full', props.className)}>
-      <thead className="font-semibold">
-        <tr className="bg-primary text-primary-foreground">
-          {header.map((cell, index) => {
-            return (
-              <td key={index} className={cn('px-4 py-2 md:px-6 md:py-4')}>
-                {cell}
-              </td>
-            );
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        {body.map((row, index) => {
-          const isEven = index % 2 === 0;
-          const cells = header.map((_, index) => {
-            const cell = row[index];
-            return (
-              <td key={index} className="px-4 py-2 md:px-6 md:py-4">
-                {cell}
-              </td>
-            );
-          });
-          return (
-            <tr
-              key={index}
-              className={cn({
-                'bg-background': isEven,
-                'bg-background1': !isEven,
-              })}
-            >
-              {cells}
-            </tr>
-          );
-        })}
-      </tbody>
-      {footer.length > 0 && (
-        <tfoot className="font-semibold">
-          <tr
-            className={cn({
-              'bg-background': hasEvenElements,
-              'bg-background1': !hasEvenElements,
-            })}
-          >
-            {header.map((_, index) => {
-              const cell = footer[index];
+    <div className="w-full overflow-x-auto">
+      <table {...props} className={cn('w-full', props.className)}>
+        <thead className="font-semibold">
+          <tr className="bg-primary text-primary-foreground">
+            {header.map((cell, index) => {
               return (
-                <td key={index} className="px-4 py-2 md:px-6 md:py-4">
+                <td key={index} className={cn('px-4 py-2 md:px-6 md:py-4')}>
                   {cell}
                 </td>
               );
             })}
           </tr>
-        </tfoot>
-      )}
-    </table>
+        </thead>
+        <tbody>
+          {body.map((row, index) => {
+            const isEven = index % 2 === 0;
+            const cells = header.map((_, index) => {
+              const cell = row[index];
+              return (
+                <td key={index} className="px-4 py-2 md:px-6 md:py-4">
+                  {cell}
+                </td>
+              );
+            });
+            return (
+              <tr
+                key={index}
+                className={cn({
+                  'bg-background': isEven,
+                  'bg-background1': !isEven,
+                })}
+              >
+                {cells}
+              </tr>
+            );
+          })}
+        </tbody>
+        {footer.length > 0 && (
+          <tfoot className="font-semibold">
+            <tr
+              className={cn({
+                'bg-background': hasEvenElements,
+                'bg-background1': !hasEvenElements,
+              })}
+            >
+              {header.map((_, index) => {
+                const cell = footer[index];
+                return (
+                  <td key={index} className="px-4 py-2 md:px-6 md:py-4">
+                    {cell}
+                  </td>
+                );
+              })}
+            </tr>
+          </tfoot>
+        )}
+      </table>
+    </div>
   );
 };
 
