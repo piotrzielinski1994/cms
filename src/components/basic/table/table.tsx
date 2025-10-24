3ce59e01a9eace397e60dbe48fa65cd830202525
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-10-24 22:29:36 +0200

    asd

--- src/components/basic/table/table.tsx
+++ src/components/basic/table/table.tsx
@@ -1,5 +1,6 @@
 import { HtmlProps } from '@/utils/html/html.types';
 import { cn } from '@/utils/tailwind';
+import { BoolMap } from '@/utils/types';
 import { forwardRef, ReactNode } from 'react';
 
 type TableProps = HtmlProps['table'] & {
@@ -8,16 +9,27 @@ type TableProps = HtmlProps['table'] & {
   footer?: ReactNode[];
 };
 
+const styles = {
+  root: 'w-full overflow-x-auto',
+  native: 'w-full',
+  header: 'font-semibold',
+  headerRow: 'bg-primary text-primary-foreground',
+  row: ({ isOdd }: BoolMap<'isOdd'>) => cn('bg-background', { 'bg-background1': isOdd }),
+  column: 'px-4 py-2 md:px-6 md:py-4',
+  footer: 'font-semibold',
+  footerRow: ({ isOdd }: BoolMap<'isOdd'>) => cn('bg-background', { 'bg-background1': !isOdd }),
+};
+
 // We always map through header columns in case body or footer are of different length
 // Static tuple declarations won't work as we always .map before passing props -> loosing tuple
 const Component = forwardRef<HTMLTableElement, TableProps>((props, ref) => {
-  const { header, body, footer = [], ...rest } = props;
+  const { header, body, footer = [], className, ...rest } = props;
   const hasEvenElements = body.length % 2 === 0;
   return (
-    <Root>
+    <Root className={className}>
       <Native ref={ref} {...rest}>
         <Header>
-          <Row className="bg-primary text-primary-foreground">
+          <Row className={styles.headerRow}>
             {header.map((cell, index) => {
               return <Column key={index}>{cell}</Column>;
             })}
@@ -25,19 +37,18 @@ const Component = forwardRef<HTMLTableElement, TableProps>((props, ref) => {
         </Header>
         <Body>
           {body.map((row, index) => {
-            const isEven = index % 2 === 0;
-            const className = cn('bg-background', { 'bg-background1': !isEven });
+            const isOdd = index % 2 !== 0;
             const cells = header.map((_, index) => {
               const cell = row[index];
               return <Column key={index}>{cell}</Column>;
             });
             // prettier-ignore
-            return <Row key={index} className={className}>{cells}</Row>;
+            return <Row key={index} className={styles.row({ isOdd })}>{cells}</Row>;
           })}
         </Body>
         {footer.length > 0 && (
           <Footer>
-            <Row className={cn('bg-background', { 'bg-background1': !hasEvenElements })}>
+            <Row className={styles.footerRow({ isOdd: hasEvenElements })}>
               {header.map((_, index) => {
                 const cell = footer[index];
                 return <Column key={index}>{cell}</Column>;
@@ -51,31 +62,27 @@ const Component = forwardRef<HTMLTableElement, TableProps>((props, ref) => {
 });
 
 const Root = ({ className, ...rest }: HtmlProps['div']) => {
-  const defaultClassNames = cn('w-full overflow-x-auto', className);
-  return <div className={defaultClassNames} {...rest} />;
+  return <div className={cn(styles.root, className)} {...rest} />;
 };
 
 const Native = forwardRef<HTMLTableElement, HtmlProps['table']>(({ className, ...rest }) => {
-  return <table className={cn('w-full', className)} {...rest} />;
+  return <table className={cn(styles.native, className)} {...rest} />;
 });
 
 const Header = ({ className, ...rest }: HtmlProps['thead']) => {
-  const defaultClassNames = cn('font-semibold', className);
-  return <thead className={defaultClassNames} {...rest} />;
+  return <thead className={cn(styles.header, className)} {...rest} />;
 };
 
 const Body = (props: HtmlProps['tbody']) => <tbody {...props} />;
 
 const Footer = ({ className, ...rest }: HtmlProps['tfoot']) => {
-  const defaultClassNames = cn('font-semibold', className);
-  return <tfoot className={defaultClassNames} {...rest} />;
+  return <tfoot className={cn(styles.footer, className)} {...rest} />;
 };
 
 const Row = (props: HtmlProps['tr']) => <tr {...props} />;
 
 const Column = ({ className, ...rest }: HtmlProps['td']) => {
-  const defaultClassNames = cn('px-4 py-2 md:px-6 md:py-4', className);
-  return <td className={defaultClassNames} {...rest} />;
+  return <td className={cn(styles.column, className)} {...rest} />;
 };
 
 const Table = Object.assign(Component, {
