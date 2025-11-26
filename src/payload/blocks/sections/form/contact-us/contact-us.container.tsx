5e302fce5c2e3d72dcfdb6eb6d1afe23575b88cd
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2025-11-26 16:34:43 +0100

    Add sending gtm event on contact-us submission

--- src/payload/blocks/sections/form/contact-us/contact-us.container.tsx
+++ src/payload/blocks/sections/form/contact-us/contact-us.container.tsx
@@ -3,7 +3,14 @@
 import { ContactUs } from '@/components/sections/form/contact-us/contact-us';
 
 const ContactUsContainer = () => {
-  return <ContactUs onSubmit={(data) => console.log('@@@ data | ', data)} />;
+  return (
+    <ContactUs
+      onSubmit={(data) => {
+        console.log('@@@ data | ', data);
+        window.dataLayer?.push({ event: 'contact_us_submit' });
+      }}
+    />
+  );
 };
 
 export { ContactUsContainer };
