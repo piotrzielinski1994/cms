8ee6ce526d159f2d409030e6794dd89f4eb26f0d
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2026-01-25 13:34:38 +0100

    asd

--- src/components/advanced/testimonial/testimonial.tsx
+++ src/components/advanced/testimonial/testimonial.tsx
@@ -1,26 +1,36 @@
 import { Image } from '@/components/basic/image/image';
+import { HtmlProps } from '@/utils/html/html.types';
 import { cn } from '@/utils/tailwind';
 
-type TestimonialProps = {
+type TestimonialProps = HtmlProps<'figure'> & {
   image: string;
   quote: string;
   name: string;
   annotation?: string;
 };
 
+const styles = {
+  root: cn('p-4 md:p-6', 'grid grid-cols-[auto_1fr] gap-4', 'bg-background1'),
+  quote: 'col-span-2 italic',
+  image: 'w-14 h-14 rounded-full',
+  content: 'grid content-center',
+  name: 'font-semibold',
+} as const;
+
 const Testimonial = (props: TestimonialProps) => {
+  const { image, quote, name, annotation, className, ...rest } = props;
   return (
-    <figure className={cn('p-4 md:p-6', 'grid grid-cols-[auto_1fr] gap-4', 'bg-background1')}>
-      <blockquote className="col-span-2 italic">
-        <p>{props.quote}</p>
+    <figure className={cn(styles.root, className)} {...rest}>
+      <blockquote className={styles.quote}>
+        <p>{quote}</p>
       </blockquote>
-      <Image src={props.image} alt={props.name} className="w-14 h-14 rounded-full" />
-      <figcaption className="grid content-center">
-        <span className="font-semibold">{props.name}</span>
-        {props.annotation && <span className="">{props.annotation}</span>}
+      <Image src={image} alt={name} className={styles.image} />
+      <figcaption className={styles.content}>
+        <span className={styles.name}>{name}</span>
+        {annotation && <span>{annotation}</span>}
       </figcaption>
     </figure>
   );
 };
 
-export { Testimonial };
+export { styles, Testimonial };
